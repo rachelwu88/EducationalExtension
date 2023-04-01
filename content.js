0753a17992be47cc8c55a5c7b9f1504c639fdba9
Anish Pandey <anishpandey234567@gmail.com>
2023-04-01 15:59:36 -0400

    fixed API thing

--- content.js
+++ content.js
@@ -3,14 +3,61 @@ console.log("Content script loaded");
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log("Message received:", request);
   if (request.action === 'brieflySummarize') {
-    chrome.runtime.sendMessage({ action: 'summarizeText', text: request.selection }, summary => {
+    summarizeText(request.selection).then(summary => {
+      console.log(summary)
       alert('Summary: ' + summary);
     });
   } else if (request.action === 'brieflyMakeQuiz') {
-    chrome.runtime.sendMessage({ action: 'makeQuiz', text: request.selection }, quiz => {
-      console.log(quiz);
+    makeQuiz(request.selection).then(quiz => {
+      console.log(quiz)
       alert('Quiz: ' + JSON.stringify(quiz));
     });
   }
 });
 
+async function summarizeText(text) {
+  const response = await fetch('https://api.openai.com/v1/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer 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'
+    },
+    body: JSON.stringify({
+      prompt: `Summarize this text in one paragraph "${text}"`,
+      model: 'text-davinci-002',
+      max_tokens: 1500
+      // n: 1,
+      // stop: null,
+      // temperature: 0.7
+    }),
+  });
+
+  const data = await response.json();
+  console.log(data)
+  const summary = data.choices[0].text.trim();
+  console.log(summary)
+  return summary;
+}
+
+
+async function makeQuiz(text) {
+  const response = await fetch('https://api.openai.com/v1/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer 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'
+    },
+    body: JSON.stringify({
+      prompt: `Create a quiz question and answer based on the following text: "${text}"`,
+      model: 'text-davinci-002'
+      // max_tokens: 300,
+      // n: 1,
+      // stop: null,
+      // temperature: 0.7
+    })
+  });
+
+  const data = await response.json();
+  const quiz = data.choices[0].text.trim();
+  return quiz;
+}
