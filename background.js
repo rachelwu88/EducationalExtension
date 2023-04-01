7789222906c0e1bcd285f3219b15a026f5f31ece
vanessanyc <elm.vanessa.louis@gmail.com>
2023-04-01 15:07:22 -0400

    summarize

--- background.js
+++ background.js
@@ -1,28 +1,31 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-      id: 'brieflySummarize',
-      title: 'Summarize',
-      contexts: ['selection']
-    });
-  
-    chrome.contextMenus.create({
-      id: 'brieflyMakeQuiz',
-      title: 'Make Quiz',
-      contexts: ['selection']
-    });
+  chrome.contextMenus.create({
+    id: 'brieflySummarize',
+    title: 'Summarize',
+    contexts: ['selection']
   });
-  
-  chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === 'brieflySummarize' || info.menuItemId === 'brieflyMakeQuiz') {
-      // Inject the content script into the current tab
-      chrome.tabs.executeScript(tab.id, { file: 'content.js' }, () => {
-        // Send a message to the content script after it's injected
+
+  chrome.contextMenus.create({
+    id: 'brieflyMakeQuiz',
+    title: 'Make Quiz',
+    contexts: ['selection']
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'brieflySummarize' || info.menuItemId === 'brieflyMakeQuiz') {
+    // Inject the content script into the current tab
+    chrome.tabs.executeScript(tab.id, { file: 'content.js' }, () => {
+      // Wait for a short delay before sending a message to the content script
+      setTimeout(() => {
         chrome.tabs.sendMessage(tab.id, {
           action: info.menuItemId,
           selection: info.selectionText
         });
         console.log("Message sent to content script:", info.selectionText);
-      });
-    }
-  });
+      }, 100);
+    });
+  }
+});
+
   
