01d8968f7195449b4d549d393be9bdc3bae6d41e
vanessanyc <elm.vanessa.louis@gmail.com>
2023-04-01 19:49:58 -0400

    same

--- background.js
+++ background.js
@@ -1,23 +1,22 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-      id: 'brieflySummarize',
-      title: 'Summarize',
-      contexts: ['selection']
-    });
-  
-    chrome.contextMenus.create({
-      id: 'brieflyMakeQuiz',
-      title: 'Make Quiz',
-      contexts: ['selection']
-    });
+  chrome.contextMenus.create({
+    id: 'brieflySummarize',
+    title: 'Summarize',
+    contexts: ['selection']
   });
-  
-  chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === 'brieflySummarize' || info.menuItemId === 'brieflyMakeQuiz') {
-      chrome.tabs.sendMessage(tab.id, {
-        action: info.menuItemId,
-        selection: info.selectionText
-      });
-    }
+
+  chrome.contextMenus.create({
+    id: 'brieflyMakeQuiz',
+    title: 'Make Quiz',
+    contexts: ['selection']
   });
-  
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'brieflySummarize' || info.menuItemId === 'brieflyMakeQuiz') {
+    chrome.tabs.sendMessage(tab.id, {
+      action: info.menuItemId,
+      selection: info.selectionText
+    });
+  }
+});
--- content.js
+++ content.js
@@ -1,40 +1,40 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'summarize') {
-      summarizeText(request.selection).then(summary => {
-        alert('Summary: ' + summary);
-      });
-    } else if (request.action === 'makeQuiz') {
-      makeQuiz(request.selection).then(quiz => {
-        alert('Quiz: ' + JSON.stringify(quiz));
-      });
-    }
-  });
-  
-  async function summarizeText(text) {
-    const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer YOUR_API_KEY'
-      },
-      body: JSON.stringify({
-        prompt: `Summarize the following text: "${text}"`,
-        max_tokens: 50,
-        n: 1,
-        stop: null,
-        temperature: 0.7
-      })
+  if (request.action === 'summarize') {
+    summarizeText(request.selection).then(summary => {
+      alert('Summary: ' + summary);
     });
-  
-    const data = await response.json();
-    const summary = data.choices[0].text.trim();
-    return summary;
+  } else if (request.action === 'makeQuiz') {
+    makeQuiz(request.selection).then(quiz => {
+      alert('Quiz: ' + JSON.stringify(quiz));
+    });
   }
-  
-  
-  
-  async function makeQuiz(text) {
-    // Call ChatGPT API for generating quiz
-    // Return the quiz
-  }
+});
+
+async function summarizeText(text) {
+  const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer YOUR_API_KEY'
+    },
+    body: JSON.stringify({
+      prompt: `Summarize the following text: "${text}"`,
+      max_tokens: 50,
+      n: 1,
+      stop: null,
+      temperature: 0.7
+    })
+  });
+
+  const data = await response.json();
+  const summary = data.choices[0].text.trim();
+  return summary;
+}
+
+
+
+async function makeQuiz(text) {
+  // Call ChatGPT API for generating quiz
+  // Return the quiz
+}
   
