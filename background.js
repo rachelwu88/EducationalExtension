6cbe9f6ab43fbc4a0e143b23f0cb378e29ad8698
anishpandey234 <83241378+anishpandey234@users.noreply.github.com>
2023-04-01 11:22:20 -0400

    Add files via upload

---
+++ background.js
@@ -0,0 +1,23 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+      id: 'brieflySummarize',
+      title: 'Briefly: Summarize',
+      contexts: ['selection']
+    });
+  
+    chrome.contextMenus.create({
+      id: 'brieflyMakeQuiz',
+      title: 'Briefly: Make Quiz',
+      contexts: ['selection']
+    });
+  });
+  
+  chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === 'brieflySummarize' || info.menuItemId === 'brieflyMakeQuiz') {
+      chrome.tabs.sendMessage(tab.id, {
+        action: info.menuItemId,
+        selection: info.selectionText
+      });
+    }
+  });
+  
